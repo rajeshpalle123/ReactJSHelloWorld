de6f691d59e56d69a7e9b7c558121e3fe250004f
rajeshpalle123 <74284302+rajeshpalle123@users.noreply.github.com>
2023-06-27 12:12:25 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-# navigate to app folder
-cd /app
-
-# install dependencies
+cd /home/ubuntu
 npm install
-
-# install create-react-app and react-scripts
-# without react-scripts application cannot be started
-npm install --save create-react-app react-scripts
-
-# install pm2 process manager
+npm install --save react react-dom react-scripts react-particles-js
 npm install pm2 -g

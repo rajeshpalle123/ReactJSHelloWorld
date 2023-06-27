81c1ad3641b30502103e017249bfee44430f13d4
rajeshpalle123 <74284302+rajeshpalle123@users.noreply.github.com>
2023-06-27 12:11:48 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# navigate to app folder
-cd /app
-
-# install node and npm
+cd /home/ubuntu/
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-apt-get -y install nodejs npm
+apt update
+apt -y install nodejs npm
